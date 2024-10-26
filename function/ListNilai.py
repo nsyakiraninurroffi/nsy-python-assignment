f9687fe86d36fc0bdc4b45f78905d0fc5b686309
= <nesyakiraninurrof@gmail.com>
2024-10-26 10:32:17 +0700

    create aplikasi ListNilai

---
+++ function/ListNilai.py
@@ -0,0 +1,26 @@
+#  Dibuat oleh : Nesya Kirani Nurroffi
+# Tanggal Pengerjaan : 26 / 10 / 2024
+# Program List Nilai
+
+nilai = []
+jumlah = int(input('Jumlah data yang akan di input : '))
+
+for i in range(jumlah):
+    nilai.append(int(input(f'Nilai ke-{i+1} : ')))
+
+#perhitungan data di list 
+total = max = 0
+min = nilai[0]
+for data in nilai:
+    total += data
+    if data > max :
+        max = data
+
+    if data < min :
+        min = data
+
+
+print(f"\ntotal")
+print(f'Rata-rata : {total / jumlah}')
+print(f'Nilai terbesar : {max}')
+print(f'Nilai terkecil : {min}')
