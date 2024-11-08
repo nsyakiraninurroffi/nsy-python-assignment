7b6898566a6a200b73583a5c6a3620f199e084a6
= <nesyakiraninurrof@gmail.com>
2024-11-08 08:27:33 +0700

    create aplikasi MenghitungDiskonBelanja

---
+++ function/MenghitungDiskonBelanja.py
@@ -0,0 +1,17 @@
+#  Dibuat oleh : Nesya Kirani Nurroffi
+# Tanggal Pengerjaan : 08 / 11 / 2024
+# Program Menghitung Diskon Bersadarkan Total Belanja
+
+print("="*40)
+print("Program Menghitung Diskon Berdasarkan Total Belanja")
+def hitung_diskon(total_belanja):
+    if total_belanja >= 100000:
+        return total_belanja * 0.1
+    return 0
+
+print("="*40)
+total_belanja = float(input("Masukkan total belanja: "))
+diskon = hitung_diskon(total_belanja)
+total_bayar = total_belanja - diskon
+print(f"Total yang harus dibayar: {total_bayar}")
+print("="*40)
