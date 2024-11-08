51e0f048f9b79e6a4f748609b094ece7cfdbbd63
= <nesyakiraninurrof@gmail.com>
2024-11-08 08:09:36 +0700

    create aplikasi MenghitungEnergiKinetik

---
+++ logic programs/MenghitungEnergiKinetik.py
@@ -0,0 +1,16 @@
+#  Dibuat oleh : Nesya Kirani Nurroffi
+# Tanggal Pengerjaan : 08 / 11 / 2024
+# Program Menghitung Energi Kinetik
+
+print("\033[1;34m=====================")
+print("====Enegri Kinetik====")
+print("=====================")
+
+m = float(input("Masukan massa/berat nya: "))
+v = float(input("Masukan volumenya: "))
+
+Ek = 1/2 * m * v * v
+
+print("\33[97m======================")
+print(f"Jadi Energi Kinetiknya = {round (Ek, 2)} m/s")
+print("\33[97m======================\n")
