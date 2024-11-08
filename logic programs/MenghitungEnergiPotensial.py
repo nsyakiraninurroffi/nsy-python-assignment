0347f7e39a30ac7e810ad24216d3925877ae720b
= <nesyakiraninurrof@gmail.com>
2024-11-08 09:06:57 +0700

    create aplikasi MenghitungEnergiPotensial

---
+++ logic programs/MenghitungEnergiPotensial.py
@@ -0,0 +1,17 @@
+#  Dibuat oleh : Nesya Kirani Nurroffi
+# Tanggal Pengerjaan : 08 / 11 / 2024
+# Program Menghitung Energi Potensial
+
+print('\n')
+print('='*40)
+print('Menghitung Energi Potensial')
+print('='*40)
+
+m = int(input('Masukan Massa Benda\t\t: '))
+g = 10
+h = int(input('Masukan Ketinggian Benda\t: '))
+
+ep = m * g * h
+
+print('Energi Potensialnya adalah\t: ', ep)
+print("="*40)
