199f28489c33f673ad61507999c381d7fb683ff8
= <nesyakiraninurrof@gmail.com>
2024-11-08 07:42:59 +0700

    create aplikasi MenghitungJarak

---
+++ logic programs/MenghitungJarak.py
@@ -0,0 +1,12 @@
+#  Dibuat oleh : Nesya Kirani Nurroffi
+# Tanggal Pengerjaan : 08 / 11 / 2024
+# Program Menghitung Jarak
+
+print("="*40)
+kecepatan = int(input("masukkan kecepatan mobil (km/jam)\t: "))
+waktu = int(input("masukkan waktu tempuh (jam)\t\t: "))
+
+jarak = kecepatan * waktu
+print("="*40)
+print(f"Jarak yang di tempuh mobil adalah {jarak} km/jam")
+print("="*40)
