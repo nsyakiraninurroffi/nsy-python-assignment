0fa5f0bc3eb467afc3fef1b189108d3071225915
= <nesyakiraninurrof@gmail.com>
2024-11-08 13:34:58 +0700

    create aplikasi CekBilanganPrima

---
+++ if else/CekBilanganPrima.py
@@ -0,0 +1,14 @@
+
+print("="*40)
+angka = int(input("Masukkan sebuah angka: "))
+
+if angka > 1:
+    for i in range(2, angka):
+        if (angka % i) == 0:
+            print("Itu Bukan bilangan prima ish.")
+            break
+    else:
+        print("Yup Itu Bilangan prima.")
+else:
+    print("Itu Bukan bilangan prima ish.")
+print("="*40)
