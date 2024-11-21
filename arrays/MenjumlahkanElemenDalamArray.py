130d7b00d80adef00dcfd52006226cc78c2a912c
= <nesyakiraninurrof@gmail.com>
2024-11-21 08:03:35 +0700

    create aplikasi MenjumlahkanElemenDalamArray

---
+++ arrays/MenjumlahkanElemenDalamArray.py
@@ -0,0 +1,15 @@
+# Dibuat oleh : Nesya Kirani Nurroffi
+# Tanggal Pengerjaan : 21 / 11 / 2024
+# Program Menjumlahkan Elemen dalam Array
+
+import os
+os.system ("cls")
+print("="*40)
+print("{program Menghitung Elemen dalam Array}")
+print("="*40)
+
+array = [1, 2, 3, 4, 5]
+print("Array:", array)
+print("Jumlah elemen:", sum(array))
+print("="*40)
+
