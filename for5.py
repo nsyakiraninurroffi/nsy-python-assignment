d8f3a00bc248b31c06c12a8a2f46cfeaa1c68eec
𝑠ℎ𝑦𝑎𝑦´ˎ˗ <nesyakiraninurroff@gmail.com>
2025-01-07 17:46:48 +0700

    Add files via upload

---
+++ for5.py
@@ -0,0 +1,8 @@
+hasil = 0
+
+for w in range(1, 6):
+    print(w)
+    hasil += w
+    
+print("___+")
+print(hasil)
---
+++ while10.py
@@ -0,0 +1,5 @@
+# 7. i love u repeated 5 times
+i = 0
+while i < 5:
+    print("i love u")
+    i += 1
